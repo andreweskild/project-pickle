1ebb8ef1cc70578ce8f480deb7a44df205d9bfca
Andrew Eskild Christensen <andreweskild@gmail.com>
2018-08-01 03:25:59 +0000

    fix bug causing preview pixels to be drawn above all layers while actively drawing

--- lib/widgets/pixels/canvas_controller.dart
+++ lib/widgets/pixels/canvas_controller.dart
@@ -5,6 +5,7 @@ import 'package:flutter_redux/flutter_redux.dart';
 import 'package:project_pickle/data_objects/tool_types.dart';
 import 'package:project_pickle/tools/tool_controller.dart';
 import 'package:project_pickle/state/app_state.dart';
+import 'package:project_pickle/widgets/pixels/pixel_canvas_layer.dart';
 
 class CanvasModel {
   CanvasModel(
@@ -31,6 +32,7 @@ class CanvasController extends StatefulWidget {
 class _CanvasControllerState extends State<CanvasController> {
   ToolController _toolController;
   int _layerCount;
+  int _currentLayerIndex;
 
 
   @override
@@ -44,12 +46,17 @@ class _CanvasControllerState extends State<CanvasController> {
         if(_layerCount == null) {
           _layerCount = store.state.layers.length;
         }
+        if(_currentLayerIndex == null) {
+          _currentLayerIndex = store.state.currentLayerIndex;
+        }
 
         store.onChange.listen(
             (state) {
-              if(state.layers.length != _layerCount) {
+              if(state.layers.length != _layerCount ||
+                state.currentLayerIndex != _currentLayerIndex) {
                 setState(() {
                   _layerCount = state.layers.length;
+                  _currentLayerIndex = state.currentLayerIndex;
                 });
               }
             }
@@ -62,6 +69,14 @@ class _CanvasControllerState extends State<CanvasController> {
         // the current gesture.
 
         int maxPointerCount = 0;
+
+        List<PixelCanvasLayer> layers;
+
+        layers = store.state.layers.getRange(0, store.state.currentLayerIndex + 1).toList();
+        layers.add(store.state.previewLayer);
+        layers.addAll(store.state.layers.getRange(store.state.currentLayerIndex + 1, store.state.layers.length));
+
+
         return Listener(
           onPointerMove: (details) {
             if (maxPointerCount == 1) {
@@ -95,12 +110,7 @@ class _CanvasControllerState extends State<CanvasController> {
               width: 32.0,
               height: 32.0,
               child: Stack(
-                children: <Widget>[
-                  Stack(
-                    children: store.state.layers,
-                  ),
-                  store.state.previewLayer,
-                ],
+                children: layers
               ),
             ),
           ),
